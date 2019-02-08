5ee52ec8cf4fc7c536e577166d9224e0e5b03719
Jeremy Fosbinder <JFosbinder@TriSourceSolutions.com>
2019-02-08 12:04:20 -0600

    na

--- Src/Render/MCD3DRenderEngine/src/Headers/WindowWrapper.h
+++ Src/Render/MCD3DRenderEngine/src/Headers/WindowWrapper.h
@@ -7,12 +7,21 @@ namespace MC {
 	class WindowWrapper {
 	public:
 		WindowWrapper(const RENDER_CONFIG& renderConfig);
-		WindowWrapper(const WindowWrapper&) = delete;
-		WindowWrapper(const WindowWrapper&&) = delete;
+		WindowWrapper(const WindowWrapper&)            = delete;
+		WindowWrapper(const WindowWrapper&&)           = delete;
 		WindowWrapper& operator=(const WindowWrapper&) = delete;
 		~WindowWrapper();
 
 		HWND Init();
+
+	private:
+
+		/*****************************************************/
+		/* INITIALIZATION HELPERS                            */
+		/*****************************************************/
+		void RegisterMCWindowClass();
+		void CreateMCWindow();
+		void EnsureValidWindowConfig();
 
 	private:
 		bool                _initialized;
--- Src/Render/MCD3DRenderEngine/src/Source/WindowWrapper.cpp
+++ Src/Render/MCD3DRenderEngine/src/Source/WindowWrapper.cpp
@@ -2,8 +2,14 @@
 #include "../../../../Common/MCLog/src/Headers/MCLog.h"
 #include "../../../../Common/MCCommon/src/Headers/Utility.h"
 
-namespace MC {
+#define MC_WINDOW_CLASS_NAME "MCWindow"
+#define MC_WINDOW_TITLE "MC Demo"
 
+// Used for error detection.
+#define MAX_VALID_WINDOW_WIDTH  10000
+#define MAX_VALID_WINDOW_HEIGHT 10000
+
+namespace MC {
 
 #pragma region window_proc
 
@@ -29,22 +35,37 @@ namespace MC {
 
 #pragma region Initialization
 
+	/*
+		Register and create a render window.
+	*/
 	HWND WindowWrapper::Init() {
-		// TODO:
-		//	This method needs to use _initialRenderConfig to make the window creation code
-		//  use the configuration options.
+		RegisterMCWindowClass();
+		CreateMCWindow();		
+
+		ShowWindow(_hwnd, SW_RESTORE);
+		UpdateWindow(_hwnd);
+		
+		_initialized = true;
+
+		return _hwnd;
+	}
+
+	/*
+		Registers the actual rendering window.
+	*/
+	void WindowWrapper::RegisterMCWindowClass() {
 		WNDCLASS wc;
 
-		wc.style = CS_HREDRAW | CS_VREDRAW;
-		wc.lpfnWndProc = WndProc;
-		wc.cbClsExtra = 0;
-		wc.cbWndExtra = 0;
-		wc.hInstance = NULL;
-		wc.hIcon = LoadIcon(0, IDI_APPLICATION);
-		wc.hCursor = LoadCursor(0, IDC_ARROW);
+		wc.style         = CS_HREDRAW | CS_VREDRAW;
+		wc.lpfnWndProc   = WndProc;
+		wc.cbClsExtra    = 0;
+		wc.cbWndExtra    = 0;
+		wc.hInstance     = NULL;
+		wc.hIcon         = LoadIcon(0, IDI_APPLICATION);
+		wc.hCursor       = LoadCursor(0, IDC_ARROW);
 		wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-		wc.lpszMenuName = 0;
-		wc.lpszClassName = "MCWindowClass";
+		wc.lpszMenuName  = 0;
+		wc.lpszClassName = MC_WINDOW_CLASS_NAME;
 
 		INIT_INFO("Registering window class.");
 
@@ -54,34 +75,54 @@ namespace MC {
 		}
 
 		INIT_INFO("Registered.");
+	}
 
+	/*
+		Create the actual rendering window.
+	*/
+	void WindowWrapper::CreateMCWindow() {
 		INIT_INFO("Creating window.");
 
+		EnsureValidWindowConfig();
+
 		_hwnd = CreateWindow(
-			"MCWindowClass",     // Class name
-			"MC",                // Window title
-			WS_OVERLAPPEDWINDOW, // style flags
-			CW_USEDEFAULT,       // x-coordinate
-			CW_USEDEFAULT,       // y-coordinate
-			CW_USEDEFAULT,       // width
-			CW_USEDEFAULT,       // height
-			0,                   // parent window
-			0,                   // menu handle
-			0,                   // app instance
-			0                    // extra creation parameters
+			MC_WINDOW_CLASS_NAME,                // Class name
+			MC_WINDOW_TITLE,                     // Window title
+			WS_OVERLAPPEDWINDOW,                 // style flags
+			CW_USEDEFAULT,                       // x-coordinate
+			CW_USEDEFAULT,                       // y-coordinate
+			_renderConfig.DISPLAY_OUTPUT_WIDTH,  // width
+			_renderConfig.DISPLAY_OUTPUT_HEIGHT, // height
+			0,                                   // parent window
+			0,                                   // menu handle
+			0,                                   // app instance
+			0                                    // extra creation parameters
 		);
 
 		if (_hwnd == 0) {
 			INIT_ERROR("Failed to create window!");
 			MCTHROW("Failed to create window!");
 		}
-		
+
 		INIT_INFO("Window created.");
+	}
 
-		ShowWindow(_hwnd, SW_RESTORE);
-		UpdateWindow(_hwnd);
+	/*
+		Examine the window width and height in _renderConfig. Throw an exception if the values do not
+		fall within an acceptable range.
+	*/
+	void WindowWrapper::EnsureValidWindowConfig() {
+		if (_renderConfig.DISPLAY_OUTPUT_WIDTH <= 0
+			|| _renderConfig.DISPLAY_OUTPUT_WIDTH > MAX_VALID_WINDOW_WIDTH) {
+			INIT_ERROR("Detected an invalid window width ({0:d}) in the config file.", _renderConfig.DISPLAY_OUTPUT_WIDTH);
+			MCTHROW("Detected an invalid window width in the config file.");
+		}
 
-		return _hwnd;
+		if (_renderConfig.DISPLAY_OUTPUT_HEIGHT <= 0
+			|| _renderConfig.DISPLAY_OUTPUT_WIDTH > MAX_VALID_WINDOW_HEIGHT) {
+			INIT_ERROR("Detected an invalid window height ({0:d}) in the config file.", _renderConfig.DISPLAY_OUTPUT_HEIGHT);
+			MCTHROW("Detected an invalid window height in the config file.");
+		}
 	}
 
 #pragma endregion
