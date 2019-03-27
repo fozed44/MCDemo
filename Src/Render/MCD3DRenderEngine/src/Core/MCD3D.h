c2b0aebf752c377a2b78e7783171757fd89179e2
fozed44 <JeremyFosbinder@Gmail.com>
2019-03-27 13:59:50 -0500

    Update MCD3D.h

--- Src/Render/MCD3DRenderEngine/src/Core/MCD3D.h
+++ Src/Render/MCD3DRenderEngine/src/Core/MCD3D.h
@@ -199,27 +199,6 @@ namespace MC {
 
 		// Set to true at the end of the Initialize method.
 		bool _initialized;
-	};
+	};	
 	
-	class MCD3D12RenderEngine {
-	public:
-		~MCD3D12RenderEngine();
-		MCD3D12RenderEngine(MCD3D12RenderEngine&) = delete;
-		MCD3D12RenderEngine(MCD3D12RenderEngine&&) = delete;
-		MCD3D12RenderEngine& operator=(MCD3D12RenderEngine&) = delete;
-		MCD3D12RenderEnging& operator=(MCD3D12RenderEngine&&) = delete;
-	private:
-		MCD3D12RenderEngine();
-	public:
-		static MCD3D12RenderEngine *pInstance;
-		static MCD3D               *pMCD3D;
-		static MCDXGI              *pMCDXGI;
-		static MCResourceManager   *pResourceManager;
-		static MCShaderManager     *pShaderManager;
-	private:
-		std::unique_ptr<MCD3D>             _pMCD3D;
-		std::unique_ptr<MCDXGI>            _pDXGI;
-		std::unique_ptr<MCResourceManager> _pResourceManager;
-		std::unique_ptr<MCShaderManager>   _pShaderManager;
-	}
 }
